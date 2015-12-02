5f1a5ff7bedecc080a6e60ae539247103a167c34
Samuel Robertson <samuel.robertson93@gmail.com>
2015-12-01 21:55:51 -0800

    x and y recived

--- application/app.js
+++ application/app.js
@@ -4,11 +4,14 @@ var http = require('http');
 var path = require('path');
 var express = require('express');
 var app = express();
-
+var PORT = process.env.PORT || 3000;
 var server = http.Server(app)
+var io = require('socket.io');
+var Player = require('./playerSchema');
 
-var io = require('socket.io')(server);
-var Player = require('playerSchema');
+var players=[];
+var socket;
+
 
 app.use(express.static('public'));
 
@@ -17,8 +20,114 @@ app.use('/', function  (req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
-var PORT = process.env.PORT || 3000;
-server.listen('Listening on port:', PORT);
+
+server.listen(PORT, function  (err) {
+  if(err) throw err;
+  init();  // starts game server listener
+});
 
 
 
+function init () {
+  players = [];
+
+  socket = io.listen(server).on('error', function  (err) {
+    console.log(err);
+  });
+
+  // Only use WebSockets
+  // socket.set('transports', ['websocket'])
+
+  setEventHandlers();
+}
+
+
+
+var setEventHandlers = function  () {
+  socket.on('connection', onSocketConnection).on('error', function  (err) {
+    console.log(err);
+  });
+
+
+}
+
+function onSocketConnection (client) {
+  console.log('New player has connected: ' + client.id)
+
+  // Listen for client disconnected
+  //client.on('disconnect', onClientDisconnect)
+
+  // Listen for new player message
+  client.on('new player', onNewPlayer)
+
+  // Listen for move player message
+  client.on('move player', onMovePlayer)
+}
+
+
+
+
+
+
+function onNewPlayer (data) {
+  // Create a new player
+  var newPlayer = new Player(data.x, data.y)
+  console.log(newPlayer);
+  newPlayer.id = this.id
+
+  // Broadcast new player to connected socket clients
+  this.broadcast.emit('new player', {id: newPlayer.id, x: newPlayer.getX(), y: newPlayer.getY()})
+
+  // Send existing players to the new player
+  var i, existingPlayer
+  for (i = 0; i < players.length; i++) {
+    existingPlayer = players[i]
+    this.emit('new player', {id: existingPlayer.id, x: existingPlayer.getX(), y: existingPlayer.getY()})
+  }
+
+  // Add new player to the players array
+  players.push(newPlayer)
+
+}
+
+
+// Player has moved
+function onMovePlayer (data) {
+  console.log(this.id);
+  // Find player in array
+  var movePlayer = playerById(this.id)
+  // Player not found
+  if (!movePlayer) {
+    //console.log('Player not found: ' + this.id)
+    return
+  }
+
+  // Update player position
+  movePlayer.setX(data.x)
+  movePlayer.setY(data.y)
+
+  // Broadcast updated position to connected socket clients
+  this.broadcast.emit('move player', {id: movePlayer.id, x: movePlayer.getX(), y: movePlayer.getY()})
+}
+
+
+
+
+
+
+
+
+
+
+// Find player by ID
+function playerById (id) {
+
+  var i
+  for (i = 0; i < players.length; i++) {
+    if (players[i].id === id) {
+      return players[i]
+    }
+  }
+
+  return false
+}
--- application/public/game.js
+++ application/public/game.js
@@ -1,105 +1,54 @@
+/* global Phaser RemotePlayer io */
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
 
-    game.load.image('sky', 'assets/sky.png');
-    game.load.image('ground', 'assets/platform.png');
-    game.load.image('star', 'assets/star.png');
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+  game.load.image('sky', 'assets/sky.png');
+  game.load.image('ground', 'assets/platform.png');
+  game.load.image('star', 'assets/star.png');
+  game.load.spritesheet('dude', './assets/dude.png', 32, 48);
+  game.load.spritesheet('player2', 'assets/dude.png', 32, 48);
 
 }
 
+var socket; // Socket connections
 var player;
 var platforms;
 var cursors;
-
+var enemies;
 var stars;
 var score = 0;
 var scoreText;
 
-function create() {
 
-    //  We're going to be using physics, so enable the Arcade Physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+function create () {
+  enemies = [];
+  socket = io.connect();
+ game.physics.startSystem(Phaser.Physics.ARCADE);
+   //add sky
+   game.add.sprite(0, 0, 'sky');
+   platforms = game.add.group();
+   platforms.enableBody = true;
 
-    //  A simple background for our game
-    game.add.sprite(0, 0, 'sky');
+   var ground = platforms.create(0, game.world.height - 64, 'ground');
+   ground.scale.setTo(2, 2);
+   ground.body.immovable = true;
 
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = game.add.group();
+   player = game.add.sprite(32, game.world.height - 150, 'dude');
+   game.physics.arcade.enable(player);
+   player.body.collideWorldBounds = true;
+   player.body.bounce.y = 0.2;
+   player.body.gravity.y = 300;
+   player.body.collideWorldBounds = true;
 
-    //  We will enable physics for any object that is created in this group
-    platforms.enableBody = true;
 
-    // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
+   cursors = game.input.keyboard.createCursorKeys();
+ }
 
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2);
-
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
-
-    //  Now let's create two ledges
-    var ledge = platforms.create(400, 400, 'ground');
-    ledge.body.immovable = true;
-
-    ledge = platforms.create(-150, 250, 'ground');
-    ledge.body.immovable = true;
-
-    // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'dude');
-
-    //  We need to enable physics on the player
-    game.physics.arcade.enable(player);
-
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 300;
-    player.body.collideWorldBounds = true;
-
-    //  Our two animations, walking left and right.
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-    //  Finally some stars to collect
-    stars = game.add.group();
-
-    //  We will enable physics for any star that is created in this group
-    stars.enableBody = true;
-
-    //  Here we'll create 12 of them evenly spaced apart
-    for (var i = 0; i < 12; i++)
-    {
-        //  Create a star inside of the 'stars' group
-        var star = stars.create(i * 70, 0, 'star');
-
-        //  Let gravity do its thing
-        star.body.gravity.y = 300;
-
-        //  This just gives each star a slightly random bounce value
-        star.body.bounce.y = 0.7 + Math.random() * 0.2;
-    }
-
-    //  The score
-    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-
-    //  Our controls.
-    cursors = game.input.keyboard.createCursorKeys();
-    
-}
-
-function update() {
-
-    //  Collide the player and the stars with the platforms
-    game.physics.arcade.collide(player, platforms);
-    game.physics.arcade.collide(stars, platforms);
-
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    game.physics.arcade.overlap(player, stars, collectStar, null, this);
-
-    //  Reset the players velocity (movement)
-    player.body.velocity.x = 0;
+ function update () {
+  game.physics.arcade.collide(player, platforms);
+  player.body.velocity.x = 0;
+  socket.emit('move player', { x: player.body.x, y: player.body.y })
 
     if (cursors.left.isDown)
     {
@@ -131,13 +80,69 @@ function update() {
 
 }
 
-function collectStar (player, star) {
-    
-    // Removes the star from the screen
-    star.kill();
 
-    //  Add and update the score
-    score += 10;
-    scoreText.text = 'Score: ' + score;
 
+
+
+
+
+
+
+
+
+
+
+
+var setEventHandlers = function  () {
+   // Socket connection successful
+  socket.on('connect', onSocketConnected);
+  socket.on('new player', onNewPlayer);
+  socket.on('move player', onMovePlayer);
+}
+
+function onSocketConnected () {
+  console.log('Connected to socket server')
+
+  // Send local player data to the game server
+  socket.emit('new player', { x: player.body.x, y: player.body.y })
+}
+
+
+
+
+
+function onNewPlayer (data) {
+ 
+  console.log('New player connected:', data.id)
+
+  // Add new player to the remote players array
+  enemies.push(new RemotePlayer(data.id, game, player, data.x, data.y))
+}
+
+
+
+function onMovePlayer (data) {
+  var movePlayer = playerById(data.id)
+  console.log(data);
+  // Player not found
+  if (!movePlayer) {
+    console.log('Player not found: ', data.id)
+    return
+  }
+
+  // Update player position
+  movePlayer.player.x = data.x
+  movePlayer.player.y = data.y
+}
+
+
+// Find player by ID
+function playerById (id) {
+  for (var i = 0; i < enemies.length; i++) {
+    if (enemies[i].player.name === id) {
+      return enemies[i]
+    }
+  }
+
+  return false
 }
